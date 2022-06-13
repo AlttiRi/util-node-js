fb771ec4e7cea77d8ae9bac6b72d814c6780939f
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2022-06-13 20:23:00 +0300

    Log test number and line

--- src/tester.js
+++ src/tester.js
@@ -11,9 +11,11 @@ import {ANSI_GRAY, ANSI_GREEN, ANSI_RED_BOLD} from "./console-colors.js";
  * report();
  */
 export class Tester {
-    constructor() {
+    constructor(printSuccess = true) {
         this.passed = [];
         this.failed = [];
+        this.printSuccess = printSuccess;
+        this.num = 0;
     }
     destructible() {
         return {
@@ -22,18 +24,24 @@ export class Tester {
         }
     }
     eq(name, result, expected) {
+        this.num++;
+        const line = new Error().stack.split("\n")[2].match(/\d+(?=:\d+$)/)[0];
+        const prefix = `[${this.num.toString().padStart(3)}][${line.padStart(4)}]`;
         const quotes = result.toString().match(/^\s|\s$/) || expected.toString().match(/^\s|\s$/);
         const q1 = quotes ? ANSI_GRAY("\"") : " ";
         const q2 = quotes ? ANSI_GRAY("\"") : "";
         if (result === expected) {
             this.passed.push(name);
-            console.log(ANSI_GREEN(`Test ${name} passed`));
+            if (!this.printSuccess) {
+                return;
+            }
+            console.log(ANSI_GREEN(`${prefix} Test ${name} passed`));
             console.log(`Result   : ${q1}${result}${q2}`);
             console.log(`Expected : ${q1}${result}${q2}`);
             console.log("---");
         } else {
             this.failed.push(name);
-            console.log(ANSI_RED_BOLD(`Test ${name} failed`));
+            console.log(ANSI_RED_BOLD(`${prefix} Test ${name} failed`));
             console.log(`Result   : ${q1}${result}${q2}`);
             console.log(`Expected : ${q1}${expected}${q2}`);
             console.log("---");
@@ -45,3 +53,10 @@ export class Tester {
         console.log(`Passed ${this.passed.length}`);
     }
 }
+
+const {eq, report} = new Tester().destructible();
+
+eq("ab0", Math.max(1, 2), 2);
+eq("ab1", " qwe  ".trim(), "qwe");
+
+report();
