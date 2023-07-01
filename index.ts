4a37292f361940f4dc97bdda1d688ce34cb420c7
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-07-01 13:43:11 +0300

    Update exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export * from "./src/console.js";
-export * from "./src/console.js";
-export * from "./src/tester.js";
+export      * from "./src/console.js";
+export      * from "./src/console-colors.js";
 export      * from "./src/filesystem.js"
 export type * from "./src/filesystem";
 export      * from "./src/filesystem-listing.js";
 export type * from "./src/filesystem-listing";
+export      * from "./src/tester.js";
