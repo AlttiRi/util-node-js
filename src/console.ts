c43f97ae9c6d42e74b9195c2db99d4d963fecd10
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-07-28 10:04:42 +0300

    Add `stdWrite` function

--- src/console.ts
+++ src/console.ts
@@ -5,3 +5,12 @@ export const restoreCursorPosition = () => process.stdout.write("\u001B[u");
 export const eraseCursorLine       = () => process.stdout.write("\u001B[K");
 
 export const moveCursorToTop = (num = 1) => process.stdout.write(`\u001B[${num}A`);
+
+export function stdWrite(text: string): Promise<void> {
+    return new Promise((resolve, reject) => process.stdout.write(text, err => {
+        if (err) {
+            reject(err);
+        }
+        resolve();
+    }));
+}
